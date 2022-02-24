dd0429dc96c734ffe2ef408f9c077855782bfe46
Karin Nordkvist <kontakt@karinnordkvist.se>
2022-02-24 13:54:20 +0100

    Test

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,31 @@
-//DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
-
 //"Draw" the chart here 👇
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'Another month',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(100, 5, 100)'],
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('chart'),
+    config
+  );
